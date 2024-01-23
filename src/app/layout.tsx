09fe57fcded95d8c27bdfcd4ccafc9143c1a968a
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-01-23 11:11:30 -0300

    feat: redirecting and persisting data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br" className={mandali.className}>
-      <body>
+    <html lang="en">
+      <body className={mandali.className}>
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
