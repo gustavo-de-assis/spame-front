01cb4c6104b37618b890cd1c78a3cbe75963a942
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-01-24 11:46:34 -0300

    feat: navbar layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,9 @@
-"use client";
-import { AuthContext } from "@/context/AuthContext";
-import { useContext } from "react";
+import Navbar from "@/components/Navbar";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
-
   return (
     <>
-      <h1>{user?.name}</h1>
-      <h2>{user?.cpf}</h2>
+      <Navbar></Navbar>
     </>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { AuthContext } from "@/context/AuthContext";
+import Image from "next/image";
+import { useContext } from "react";
+
+export default function Navbar() {
+  const { user } = useContext(AuthContext);
+
+  const logo = require("@/assets/logo_spame.png");
+  const userLogo = require("@/assets/user.png");
+
+  return (
+    <nav className="w-screen h-28 bg-secondary flex flex-row justify-between px-6">
+      <aside className="flex flex-row items-center">
+        <Image src={logo} width={80} alt="logo" />
+        <h1 className="text-primary text-2xl ml-4"> Clínica Médica</h1>
+      </aside>
+      <aside className="flex flex-row items-center text-primary">
+        <div className="flex flex-col text-right mr-2">
+          <h1 className="text-xl">{user?.name}</h1>
+          <p className="text-sm">Sair</p>
+        </div>
+        <Image src={userLogo} width={80} alt="user" />
+      </aside>
+    </nav>
+  );
+}
