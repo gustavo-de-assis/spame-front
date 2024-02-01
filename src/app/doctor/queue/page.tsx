f9ec10a07f2400259014600f41364f68d9b3bcea
Esdras Antero <esdrasanterofontes@gmail.com>
2024-02-01 11:31:01 -0300

    fix: employee list layout

--- src/app/doctor/queue/page.tsx
+++ src/app/doctor/queue/page.tsx
@@ -3,7 +3,7 @@ import PatientQeue from "../components/PatientQueue";
 export default function Queue() {
   return (
     <main>
-      <h1 className="text-4xl text-primary">Fila de Espera</h1>
+      <h1 className="text-4xl mb-8 text-primary">Fila de Espera</h1>
       <PatientQeue />
     </main>
   );
